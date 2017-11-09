e6159ed3c20564ffc221b67250f29725c65e7ece
Nguyen Hoang Phuc <phucnguyenhoang1985@gmail.com>
2017-11-09 22:34:09 +0900

    updated layout

---
+++ _next/da5564f7-e1b3-49f2-906a-8cd4507e6051/page/_error/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/_error', function() {
+            var comp = module.exports=webpackJsonp([4],[],[342]);
+            return { page: comp.default }
+          })
+        
