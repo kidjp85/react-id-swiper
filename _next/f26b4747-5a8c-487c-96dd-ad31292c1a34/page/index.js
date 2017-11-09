4cf431981ac51f2a301e92a1dd6b37c7d6bc59e4
Nguyen Hoang Phuc <phucnguyenhoang1985@gmail.com>
2017-11-09 23:01:49 +0900

    fixed bug

---
+++ _next/f26b4747-5a8c-487c-96dd-ad31292c1a34/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([3],[],[341]);
+            return { page: comp.default }
+          })
+        
