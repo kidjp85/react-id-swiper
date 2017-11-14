82211e390deecbcb8dcb2662bf64b552d9ccc2ee
Nguyen Hoang Phuc <nhphuc85@gmail.com>
2017-11-15 00:02:31 +0900

    added new demos (#102)

--- _next/4a3ee107-9ee9-46d3-8874-0412a5399157/page/_error/index.js
+++ _next/4a3ee107-9ee9-46d3-8874-0412a5399157/page/_error/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([4],[],[352]);
+            var comp = module.exports=webpackJsonp([4],[],[354]);
             return { page: comp.default }
           })
         
