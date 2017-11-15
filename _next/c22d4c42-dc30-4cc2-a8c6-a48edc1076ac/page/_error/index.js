31f6fbb6ec79b6728396aded35cf7dfd357ec70f
Nguyen Hoang Phuc <nhphuc85@gmail.com>
2017-11-15 22:18:07 +0900

    added new demos for v1.5.5 (#104)

--- _next/c22d4c42-dc30-4cc2-a8c6-a48edc1076ac/page/_error/index.js
+++ _next/c22d4c42-dc30-4cc2-a8c6-a48edc1076ac/page/_error/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([4],[],[354]);
+            var comp = module.exports=webpackJsonp([4],[],[356]);
             return { page: comp.default }
           })
         
--- _next/c22d4c42-dc30-4cc2-a8c6-a48edc1076ac/page/index.js
+++ _next/c22d4c42-dc30-4cc2-a8c6-a48edc1076ac/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([3],[],[353]);
+            var comp = module.exports=webpackJsonp([3],[],[355]);
             return { page: comp.default }
           })
         
