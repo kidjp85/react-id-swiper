0848efd371e50c01c35bc79a4cd669a7427d0b65
Nguyen Hoang Phuc <nhphuc85@gmail.com>
2017-12-20 22:50:22 +0900

    updated demo for v1.5.7 (#123)

--- _next/f6e4f884-1116-48f0-ae01-da661ddf1906/page/index.js
+++ _next/f6e4f884-1116-48f0-ae01-da661ddf1906/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([3],[],[355]);
+            var comp = module.exports=webpackJsonp([3],[],[357]);
             return { page: comp.default }
           })
         
