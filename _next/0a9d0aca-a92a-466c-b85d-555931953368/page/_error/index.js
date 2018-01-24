203fdc1b739c2e65eb46460169acf76ff3524550
Nguyen Hoang Phuc <phucnguyenhoang1985@gmail.com>
2018-01-24 22:47:08 +0900

    upgrade demo 1.5.8

--- _next/0a9d0aca-a92a-466c-b85d-555931953368/page/_error/index.js
+++ _next/0a9d0aca-a92a-466c-b85d-555931953368/page/_error/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([4],[],[358]);
+            var comp = module.exports=webpackJsonp([4],[],[360]);
             return { page: comp.default }
           })
         
--- _next/0a9d0aca-a92a-466c-b85d-555931953368/page/index.js
+++ _next/0a9d0aca-a92a-466c-b85d-555931953368/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([3],[],[357]);
+            var comp = module.exports=webpackJsonp([3],[],[359]);
             return { page: comp.default }
           })
         
