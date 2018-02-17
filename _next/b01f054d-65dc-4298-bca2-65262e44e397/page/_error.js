e0995da811e27db3558532a61c418de980ea1a0a
Nguyen Hoang Phuc <nhphuc85@gmail.com>
2018-02-17 21:43:17 +0900

    fixed polyfill (#143)

---
+++ _next/b01f054d-65dc-4298-bca2-65262e44e397/page/_error.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/_error', function() {
+            var comp = module.exports=webpackJsonp([4],[],[648]);
+            return { page: comp.default }
+          })
+        
---
+++ _next/b01f054d-65dc-4298-bca2-65262e44e397/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([3],[],[647]);
+            return { page: comp.default }
+          })
+        
