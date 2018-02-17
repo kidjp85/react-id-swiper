da7d0061088ec55efcbe209f364fe2f46dfc55ff
Nguyen Hoang Phuc <nhphuc85@gmail.com>
2018-02-17 21:34:24 +0900

    demo page for 1.6.1 (#142)

---
+++ _next/ddd407ab-d108-4c5d-a460-9e5e23605173/page/_error.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/_error', function() {
+            var comp = module.exports=webpackJsonp([4],[],[353]);
+            return { page: comp.default }
+          })
+        
---
+++ _next/ddd407ab-d108-4c5d-a460-9e5e23605173/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([3],[],[352]);
+            return { page: comp.default }
+          })
+        
