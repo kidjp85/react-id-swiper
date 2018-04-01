daa3227fe93672bbbc6ef7159d00f5ab762ff2f0
Nguyen Hoang Phuc <phucnguyenhoang1985@gmail.com>
2018-04-01 22:10:02 +0900

    upgrade swiper@4.2.2

--- __tests__/ReactIdSwiper.test.js
+++ __tests__/ReactIdSwiper.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactIdSwiper from '../src/index';
-import { render } from 'enzyme'
+import { render, mount, shallow } from 'enzyme'
+import { spyOn } from 'jest'
 
 const renderSwiper = params => render(
   <ReactIdSwiper {...params}>
@@ -8,133 +9,180 @@ const renderSwiper = params => render(
     <div>Slide 2</div>
   </ReactIdSwiper>
 )
-  
+
+const mountSwiper = params => mount(
+  <ReactIdSwiper {...params}>
+    <div>Slide 1</div>
+    <div>Slide 2</div>
+  </ReactIdSwiper>
+)
+
 describe('ReactIdSwiper', () => {
-  describe('Default', () => {
-    it('should render default swiper', () => {
-      expect(renderSwiper()).toMatchSnapshot();
-    });
-  })
+  describe('defaultProps', () => {
+    const component = mountSwiper({})
 
-  describe('Pagination', () => {
-    const params = {
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-      }
-    }
-
-    it('should render pagination', () => {
-      expect(renderSwiper(params)).toMatchSnapshot();
-    })
-
-    it('should render pagination with customized class name', () => {
-      const customizedParams = {
-        ...params,
-        paginationCustomizedClass: 'pagination-with-custom-class-name'
-      }
-
-      expect(renderSwiper(customizedParams)).toMatchSnapshot();
-    })
-
-    it('should render pagination with customized function', () => {
-      const customizedParams = {
-        ...params,
-        renderCustomPagination: () => <span className="pagination-with-custom-function"></span>
-      }
-
-      expect(renderSwiper(customizedParams)).toMatchSnapshot();
+    it('should have default props', () => {
+      expect(component.prop('containerClass')).toEqual('swiper-container')
+      expect(component.prop('wrapperClass')).toEqual('swiper-wrapper')
+      expect(component.prop('slideClass')).toEqual('swiper-slide')
+      expect(component.prop('ContainerEl')).toEqual('div')
+      expect(component.prop('WrapperEl')).toEqual('div')
     })
   })
 
-  describe('Navigation', () => {
-    const params = {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-      }
-    }
-
-    it('should render navigation buttons', () => {
-      expect(renderSwiper(params)).toMatchSnapshot();
-    })
-
-    it('should render pagination with customized class name', () => {
-      const customizedParams = {
-        ...params,
-        nextButtonCustomizedClass: 'next-button-with-customized-class-name',
-        prevButtonCustomizedClass: 'prev-button-with-customized-class-name'
-      }
-
-      expect(renderSwiper(customizedParams)).toMatchSnapshot();
-    })
-
-    it('should render pagination with customized function', () => {
-      const customizedParams = {
-        ...params,
-        renderCustomNextButton: () => <span>Customized next button</span>,
-        renderCustomPrevButton: () => <span>Customized prev button</span>
-      }
-
-      expect(renderSwiper(customizedParams)).toMatchSnapshot();
+  describe('lifecycle', () => {
+    describe('componentDidMount', () => {
+      it('should initialize swiper', () => {
+        const component = mountSwiper({})
+        expect(component.instance().swiper).toBeDefined()
+      })
     })
   })
 
-  describe('Parallax', () => {
-    const params = {
-      parallax: true,
-      parallaxEl: {
-        el: '.parallax-bg',
-        value: '-23%'
+  describe('rendering snapshot', () => {
+    // With default props
+    describe('Default', () => {
+      it('should render default swiper', () => {
+        expect(renderSwiper()).toMatchSnapshot();
+      });
+    })
+
+    // Render pagination
+    describe('Pagination', () => {
+      const params = {
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        }
       }
-    }
-
-    it('should render parallax', () => {
-      expect(renderSwiper(params)).toMatchSnapshot();
+  
+      it('should render pagination', () => {
+        expect(renderSwiper(params)).toMatchSnapshot();
+      })
+  
+      it('should render pagination with customized class name', () => {
+        const customizedParams = {
+          ...params,
+          paginationCustomizedClass: 'pagination-with-custom-class-name'
+        }
+  
+        expect(renderSwiper(customizedParams)).toMatchSnapshot();
+      })
+  
+      it('should render pagination with customized function', () => {
+        const customizedParams = {
+          ...params,
+          renderCustomPagination: () => <span className="pagination-with-custom-function"></span>
+        }
+  
+        expect(renderSwiper(customizedParams)).toMatchSnapshot();
+      })
     })
-  })
 
-  describe('Scrollbar', () => {
-    const params = {
-      scrollbar: {
-        el: '.swiper-scrollbar',
-        hide: true,
+    // Render navigation
+    describe('Navigation', () => {
+      const params = {
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        }
       }
-    }
-
-    it('should render scrollbar', () => {
-      expect(renderSwiper(params)).toMatchSnapshot();
+  
+      it('should render navigation buttons', () => {
+        expect(renderSwiper(params)).toMatchSnapshot();
+      })
+  
+      it('should render pagination with customized class name', () => {
+        const customizedParams = {
+          ...params,
+          nextButtonCustomizedClass: 'next-button-with-customized-class-name',
+          prevButtonCustomizedClass: 'prev-button-with-customized-class-name'
+        }
+  
+        expect(renderSwiper(customizedParams)).toMatchSnapshot();
+      })
+  
+      it('should render pagination with customized function', () => {
+        const customizedParams = {
+          ...params,
+          renderCustomNextButton: () => <span>Customized next button</span>,
+          renderCustomPrevButton: () => <span>Customized prev button</span>
+        }
+  
+        expect(renderSwiper(customizedParams)).toMatchSnapshot();
+      })
     })
-  })
 
-  describe('No swiping', () => {
-    const params = {
-      noSwiping: true
-    }
-
-    it('should render slide with swiper-no-swiping class name', () => {
-      expect(renderSwiper(params)).toMatchSnapshot();
+    describe('Parallax', () => {
+      const params = {
+        parallax: true,
+        parallaxEl: {
+          el: '.parallax-bg',
+          value: '-23%'
+        }
+      }
+  
+      it('should render parallax', () => {
+        expect(renderSwiper(params)).toMatchSnapshot();
+      })
     })
-  })
 
-  describe('RTL', () => {
-    const params = {
-      rtl: true
-    }
-
-    it('should render with rtl', () => {
-      expect(renderSwiper(params)).toMatchSnapshot();
+    describe('Parallax', () => {
+      const params = {
+        parallax: true,
+        parallaxEl: {
+          el: '.parallax-bg',
+          value: '-23%'
+        }
+      }
+  
+      it('should render parallax', () => {
+        expect(renderSwiper(params)).toMatchSnapshot();
+      })
     })
-  })
-
-  describe('Custom container & wrapper', () => {
-    const params = {
-      ContainerEl: 'section',
-      WrapperEl: 'section'
-    }
-
-    it('should render with custom container & wrapper', () => {
-      expect(renderSwiper(params)).toMatchSnapshot();
+  
+    describe('Scrollbar', () => {
+      const params = {
+        scrollbar: {
+          el: '.swiper-scrollbar',
+          hide: true,
+        }
+      }
+  
+      it('should render scrollbar', () => {
+        expect(renderSwiper(params)).toMatchSnapshot();
+      })
+    })
+  
+    describe('No swiping', () => {
+      const params = {
+        noSwiping: true
+      }
+  
+      it('should render slide with swiper-no-swiping class name', () => {
+        expect(renderSwiper(params)).toMatchSnapshot();
+      })
+    })
+  
+    describe('RTL', () => {
+      const params = {
+        rtl: true
+      }
+  
+      it('should render with rtl', () => {
+        expect(renderSwiper(params)).toMatchSnapshot();
+      })
+    })
+  
+    describe('Custom container & wrapper', () => {
+      const params = {
+        ContainerEl: 'section',
+        WrapperEl: 'section'
+      }
+  
+      it('should render with custom container & wrapper', () => {
+        expect(renderSwiper(params)).toMatchSnapshot();
+      })
     })
   })
 })
--- test-setup.js
+++ test-setup.js
@@ -1,5 +1,26 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+const { JSDOM } = require('jsdom')
 
-configure({ adapter: new Adapter() });
 
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>')
+const { window } = jsdom
+
+const copyProps = (src, target) => {
+  const props = Object.getOwnPropertyNames(src)
+  .filter(prop => typeof target[prop] === 'undefined')
+  .reduce((result, prop) => ({
+    ...result,
+    [prop]: Object.getOwnPropertyDescriptor(src, prop),
+  }), {})
+  Object.defineProperties(target, props)
+}
+
+global.window = window
+global.document = window.document
+global.navigator = {
+  userAgent: 'node.js',
+}
+
+configure({ adapter: new Adapter() })
+copyProps(window, global)
