114b5ae0a94566d74e10aa11b47ff647a2acf668
Nguyen Hoang Phuc <nhphuc85@gmail.com>
2018-05-04 09:26:16 +0900

    updated code (#158)

--- _next/09c1db76-767a-4b37-9a0f-9a5777bc4a15/page/_error.js
+++ _next/09c1db76-767a-4b37-9a0f-9a5777bc4a15/page/_error.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([4],[],[648]);
+            var comp = module.exports=webpackJsonp([4],[],[654]);
             return { page: comp.default }
           })
         
--- _next/b01f054d-65dc-4298-bca2-65262e44e397/page/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([3],[],[647]);
-            return { page: comp.default }
-          })
-        
