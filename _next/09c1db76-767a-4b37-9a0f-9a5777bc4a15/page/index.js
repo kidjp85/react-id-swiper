32817f1393c50f68c58a530479b13bb15970e0d6
Nguyen Hoang Phuc <phucnguyenhoang1985@gmail.com>
2018-05-04 09:24:07 +0900

    updated code

--- _next/09c1db76-767a-4b37-9a0f-9a5777bc4a15/page/index.js
+++ _next/09c1db76-767a-4b37-9a0f-9a5777bc4a15/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([3],[],[359]);
+            var comp = module.exports=webpackJsonp([3],[],[653]);
             return { page: comp.default }
           })
         
--- _next/ddd407ab-d108-4c5d-a460-9e5e23605173/page/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([3],[],[352]);
-            return { page: comp.default }
-          })
-        
