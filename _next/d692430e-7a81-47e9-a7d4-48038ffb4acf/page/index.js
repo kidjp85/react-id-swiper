f1d3d60d89d2d4026e91b6433a425753b14167cd
Nguyen Hoang Phuc <nhphuc85@gmail.com>
2018-06-14 08:12:55 +0900

    updated demo for 1.6.5 (#175)

--- _next/d692430e-7a81-47e9-a7d4-48038ffb4acf/page/index.js
+++ _next/d692430e-7a81-47e9-a7d4-48038ffb4acf/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([3],[],[653]);
+            var comp = module.exports=webpackJsonp([3],[],[652]);
             return { page: comp.default }
           })
         
