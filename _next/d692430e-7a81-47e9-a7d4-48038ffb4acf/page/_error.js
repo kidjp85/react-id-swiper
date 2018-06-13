5eefa0940eee2d1c920aa35f0170ba8bbda1ae78
Nguyen Hoang Phuc <phucnguyenhoang1985@gmail.com>
2018-06-14 08:12:22 +0900

    updated demo for 1.6.5

--- _next/d692430e-7a81-47e9-a7d4-48038ffb4acf/page/_error.js
+++ _next/d692430e-7a81-47e9-a7d4-48038ffb4acf/page/_error.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([4],[],[654]);
+            var comp = module.exports=webpackJsonp([4],[],[653]);
             return { page: comp.default }
           })
         
