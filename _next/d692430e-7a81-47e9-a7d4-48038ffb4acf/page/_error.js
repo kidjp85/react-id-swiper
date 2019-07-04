8cd2a38f2508f99fe2f9f6367b4fa2b0e2f95bf7
Asher Nguyen <phucnguyenhoang1985@gmail.com>
2019-07-04 21:38:57 +1000

    updated new static page

--- _next/d692430e-7a81-47e9-a7d4-48038ffb4acf/page/_error.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([4],[],[653]);
-            return { page: comp.default }
-          })
-        
